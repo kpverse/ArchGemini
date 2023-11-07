1119c040cc5dd9d0abb87da4af5cbbf4ac18b4cd
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2023-11-08 01:05:54 +0530

    Update

--- front-end/src/components/common/MoreLinks.tsx
+++ front-end/src/components/common/MoreLinks.tsx
@@ -6,20 +6,33 @@ export default function MoreLinks() {
     return (
         <div className={style["more-links"]}>
             <div className={style["content"]}>
-                <Link className={style["link"]} href={rootPath + "/about"}>
-                    About
-                </Link>
-                <span className={style["separator"]}>·</span>
-                <Link
-                    className={style["link"]}
-                    href={"https://kpverse.in/archgemini/feedback"}
-                >
-                    Feedback
-                </Link>
-                <span className={style["separator"]}>·</span>
-                <Link className={style["link"]} href={rootPath + "/tutorial"}>
-                    Tutorial
-                </Link>
+                <div className={style["links"]}>
+                    <Link className={style["link"]} href={rootPath + "/about"}>
+                        About
+                    </Link>
+                    <span className={style["separator"]}>·</span>
+                    <Link
+                        className={style["link"]}
+                        href={"https://kpverse.in/archgemini/feedback"}
+                    >
+                        Feedback
+                    </Link>
+                    <span className={style["separator"]}>·</span>
+                    <Link
+                        className={style["link"]}
+                        href={rootPath + "/tutorial"}
+                    >
+                        Tutorial
+                    </Link>
+                    <span className={style["separator"]}>·</span>
+                </div>
+                <span className={style["copyright"]}>
+                    &copy;{" "}
+                    <Link target={"_blank"} href={"https://kpverse.in"}>
+                        KPVERSE
+                    </Link>
+                    . All Rights Reserved.
+                </span>
             </div>
         </div>
     );
